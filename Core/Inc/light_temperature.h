99a70e08cca1998199968e7c76717f88e317afb4
Bitter Gitter <62151611+Ventuuu@users.noreply.github.com>
2026-07-04 14:36:24 +0200

    light temperature in Kelvin

---
+++ Core/Inc/light_temperature.h
@@ -0,0 +1,20 @@
+#include "as7341_driver.h"
+
+#define SAMPLE_SIZE 10
+#define CHANNEL_COUNT 8
+#define PURPLE [118, 0, 237]
+#define BLUE [0, 0, 140]
+#define LIGHT_BLUE [0, 40, 255]
+#define GREEN [0, 213, 255]
+#define LIGHT_GREEN [31, 255, 0]
+#define YELLOW [255, 223, 0]
+#define ORANGE [255, 79, 0]
+#define RED [255, 0, 0]
+#define RGB_COUNT 3
+
+void get_relative_light_colors(uint16_t sample_count);
+int * get_RGB(float * rel_ch);
+uint16_t RGB_to_temperature(int * RGB_total)
+int * remove_outsiders(int * temps);
+int get_light_temperature(uint16_t * samples, uint16_t sample_number);
+
